c4182c2ceac6596b7e2b22e3283898442c5512d4
Tulit <tulitpal@gmail.com>
2021-06-24 03:10:14 +0530

    Commit before  final run

--- src/main/java/sap/Task2.java
+++ src/main/java/sap/Task2.java
@@ -1,43 +1,56 @@
 package sap;
 
-import java.io.BufferedReader;
+import static java.util.concurrent.TimeUnit.MINUTES;
+
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ScheduledFuture;
 
 public class Task2 {
 
-	public static void main(String args[]) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			System.out.println("Please enter the token:\n");
-			String token = "FortifyToken " + br.readLine().trim();
-			System.out.println("token: " + token);
-			ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-			String filename = "MaintainanceModeLog.txt";
-			FileWriter fw = new FileWriter(filename, true);
-			fw.write("Application started from " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + " Monitoring has begun\n");
-			fw.close();
-			Runnable task = new Runnable() {
-				
-				public void run() {
-					try {
-						ExternalRestHelper.checkApplicationState();
-					} catch (IOException e) {
-						System.out.println("Encountered exception at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
-						e.printStackTrace();
-					}
-				}
-			};   
-			executor.schedule(task, 60, TimeUnit.SECONDS);
-		} catch (IOException e) {
-			System.out.println("Encounted error while making REST call to create user (Task 1)");
-			e.printStackTrace();
-		}
-	}
+    private final static ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
+    public static void main(String args[]) {
+        try {
+            final String filename = "MaintainanceModeLog.txt";
+            FileWriter openingNotes = new FileWriter(filename, true);
+            openingNotes.write("Application started at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + "- Monitoring has begun\n");
+            openingNotes.close();
+
+            final Runnable task = new Runnable() {
+                public void run() {
+                    try {
+                        ExternalRestHelper.checkApplicationState();
+                    } catch (Exception e) {
+                        System.out.println("Encountered exception inside run() at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            final ScheduledFuture<?> executorHandle = executor.scheduleAtFixedRate(task, 10, 10, MINUTES);
+            executor.schedule(new Runnable() {
+                public void run() {
+                    executorHandle.cancel(true);
+                    try {
+                        FileWriter endingNotes = new FileWriter(filename, true);
+                        endingNotes.write("Application ended at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + "- Monitoring has stopped\n");
+                        endingNotes.close();
+                    } catch (Exception e) {
+                        System.out.println("Encounted exception inside executorHandle at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
+                        e.printStackTrace();
+                    }
+                    executor.shutdown();
+                }
+            }, 40, MINUTES);
+
+        } catch (IOException e) {
+            System.out.println("Encounted exception at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
+            e.printStackTrace();
+        }
+    }
 }
