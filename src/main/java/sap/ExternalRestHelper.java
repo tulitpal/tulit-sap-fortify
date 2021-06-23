e6fe95bcffb6875c22eef4a5abfeac2a880c3e62
Tulit <tulitpal@gmail.com>
2021-06-24 03:21:39 +0530

    Changing format

--- src/main/java/sap/ExternalRestHelper.java
+++ src/main/java/sap/ExternalRestHelper.java
@@ -13,21 +13,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ExternalRestHelper {
 
-	public static void checkApplicationState() throws IOException {
-		String command = "curl -X GET \"https://mini.fortify-dev.tools.sap/ssc/api/v1/applicationState\" -H \"accept: application/json\" -H \"Authorization: FortifyToken YmQwMzA5Y2QtYzJhNy00YTQ1LWE3Y2ItZGIwYWM2ZmNhZDVh\"";// "curl
-		Process process = Runtime.getRuntime().exec(command);
+    public static void checkApplicationState() throws IOException {
+        String command = "curl -X GET \"https://mini.fortify-dev.tools.sap/ssc/api/v1/applicationState\" -H \"accept: application/json\" -H \"Authorization: FortifyToken YmQwMzA5Y2QtYzJhNy00YTQ1LWE3Y2ItZGIwYWM2ZmNhZDVh\"";// "curl
+        Process process = Runtime.getRuntime().exec(command);
 
-		InputStream inputStream = process.getInputStream();
-		String text = IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
-		System.out.println("Response received: " + text);
-		ApplicationStateDto dto = new ObjectMapper().readValue(text, ApplicationStateDto.class);
-		if (dto.getData().getMaintenanceMode()) {
-			String filename = "MaintainanceModeLog.txt";
-			FileWriter fw = new FileWriter(filename, true);
-			String log = "ALERT CREATED ||| Received at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + " ||| Response from application-state-controller: " + text;
-			System.out.println(log);
-			fw.write(log + "\n");
-			fw.close();
-		}
-	}
+        InputStream inputStream = process.getInputStream();
+        String text = IOUtils.toString(inputStream, StandardCharsets.UTF_8.name());
+        System.out.println("Response received: " + text);
+        ApplicationStateDto dto = new ObjectMapper().readValue(text, ApplicationStateDto.class);
+        if (dto.getData().getMaintenanceMode()) {
+            String filename = "MaintainanceModeLog.txt";
+            FileWriter fw = new FileWriter(filename, true);
+            String log = "ALERT CREATED ||| Received at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + " ||| Response from application-state-controller: " + text;
+            System.out.println(log);
+            fw.write(log + "\n");
+            fw.close();
+        }
+    }
 }
--- src/main/java/sap/Task2.java
+++ src/main/java/sap/Task2.java
@@ -18,7 +18,7 @@ public class Task2 {
         try {
             final String filename = "MaintainanceModeLog.txt";
             FileWriter openingNotes = new FileWriter(filename, true);
-            openingNotes.write("Application started at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + "- Monitoring has begun\n");
+            openingNotes.write("Application started at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + " - Monitoring has begun\n");
             openingNotes.close();
 
             final Runnable task = new Runnable() {
@@ -38,7 +38,7 @@ public class Task2 {
                     executorHandle.cancel(true);
                     try {
                         FileWriter endingNotes = new FileWriter(filename, true);
-                        endingNotes.write("Application ended at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + "- Monitoring has stopped\n");
+                        endingNotes.write("Application ended at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) + " - Monitoring has stopped\n");
                         endingNotes.close();
                     } catch (Exception e) {
                         System.out.println("Encounted exception inside executorHandle at " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
